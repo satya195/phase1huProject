1a223f2911ee7bb91f83e2b72696ec6f609f3151
Satya m <satyam@Satyas-MacBook-Pro.local>
2025-04-13 00:43:54 +0530

    New Branch dummy Push

--- Backend /src/index.js
+++ Backend /src/index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('Hello, World');
 });
 
 app.listen(PORT, () => {
